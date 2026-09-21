b9f0a22ad00cba375801ce1ae43e57ea6d2d23a4
kien nguyen <kien34217@gmail.com>
2026-09-21 10:39:41 +0700

    config email

--- server/service/email-service.js
+++ server/service/email-service.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
+
+const dns = require("dns");
+// 👉 Ép Node.js luôn dùng IPv4, fix triệt để lỗi IPv6 trên Render / Railway / Cloud
+if (dns.setDefaultResultOrder) {
+  dns.setDefaultResultOrder("ipv4first");
+}
 const transpost = nodemailer.createTransport({
-   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4,
+  service: "gmail", 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -21,7 +24,7 @@ const ServiceEmail = async (data) => {
     });
     return result;
   } catch (error) {
-    throw error
+    throw error;
   }
 };
 module.exports = ServiceEmail;
