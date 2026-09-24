cc7b7a8b4c8ebabb67c22d2f672792e63bbdceda
kien nguyen <kien34217@gmail.com>
2026-09-25 05:42:34 +0700

    modifer API email

--- server/service/email-service.js
+++ server/service/email-service.js
@@ -1,23 +1,12 @@
 require("dotenv").config();
-const nodemailer = require("nodemailer");
 
-const dns = require("dns");
-// 👉 Ép Node.js luôn dùng IPv4, fix triệt để lỗi IPv6 trên Render / Railway / Cloud
-if (dns.setDefaultResultOrder) {
-  dns.setDefaultResultOrder("ipv4first");
-}
-const transpost = nodemailer.createTransport({
-  service: "gmail", 
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const { Resend } = require("resend");
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 const ServiceEmail = async (data) => {
   try {
-    const result = await transpost.sendMail({
-      from: `"Nexora LMS" <${process.env.EMAIL_USER}>`,
+    const result = await resend.emails.send({
+      from: "Learnova <noreply@learnova.top>",
       to: data.email,
       subject: data.subject,
       html: data.html,
